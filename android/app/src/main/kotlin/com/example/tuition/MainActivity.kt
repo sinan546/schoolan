535d64e712b61dedbd618ec9bd376fc8ab9fac81
sinan546 <sinanharamine546@gmail.com>
2022-04-01 14:49:31 +0600

    all file  commit

---
+++ android/app/src/main/kotlin/com/example/tuition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tuition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+//MyHomePage
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [Text('hello'), Text('data')],
+      ),
+    );
+  }
+}
